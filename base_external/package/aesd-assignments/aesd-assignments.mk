0473ca1120159522e0f4b9c09010fdb013554cd5
Carlos Alvarado Martinez <carlos-alvarado@outlook.com>
2024-09-21 14:03:13 -0400

    assignment 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = b704120
+AESD_ASSIGNMENTS_VERSION = 18837ef
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
